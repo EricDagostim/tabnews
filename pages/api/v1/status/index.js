214c0bdb69c46a70b2ce2048261875221bf1a04d
Éric Dagostim do Nascimento <69852409+EricDagostim@users.noreply.github.com>
2025-09-08 15:15:40 +0000

    Feat/ Adiciona select de versões do postgres e informações de atividade do banco

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,8 +1,25 @@
 import database from "infra/database.js";
 
 async function status(request, response) {
-  const result = await database.query("SELECT 1 + 1;");
-  response.status(200).json({ chave: "valor" });
+  const updatedAt = new Date().toISOString();
+
+  var pgStatement = await database.query(
+    `SELECT 
+      version() as versao,
+      (SELECT COUNT(*) FROM pg_stat_activity) AS conexoes_abertas,
+      (SELECT COUNT(*) FROM pg_stat_activity WHERE state = 'active') as conexoes_ativas,
+      current_setting('max_connections')::int AS conexoes_maximas;
+    `,
+  );
+
+  var dados = pgStatement.rows[0];
+  response.status(200).json({
+    updated_at: updatedAt,
+    postgres_version: dados.versao,
+    active_connections: parseInt(dados.conexoes_ativas),
+    opened_connections: parseInt(dados.conexoes_abertas),
+    max_connections: dados.conexoes_maximas,
+  });
 }
 
 export default status;
--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -1,4 +1,22 @@
 test("GET to /api/v1/status should return 200", async () => {
   const response = await fetch("http://localhost:3000/api/v1/status");
   expect(response.status).toBe(200);
+
+  const responseBody = await response.json();
+  expect(responseBody.updated_at).toBeDefined();
+
+  parsedUpdatedAt = new Date(responseBody.updated_at).toISOString();
+  expect(responseBody.updated_at).toEqual(parsedUpdatedAt);
+
+  expect(responseBody.postgres_version).toBeDefined();
+  expect(typeof responseBody.postgres_version).toBe("string");
+
+  expect(responseBody.active_connections).toBeDefined();
+  expect(typeof responseBody.active_connections).toBe("number");
+
+  expect(responseBody.opened_connections).toBeDefined();
+  expect(typeof responseBody.opened_connections).toBe("number");
+
+  expect(responseBody.max_connections).toBeDefined();
+  expect(typeof responseBody.max_connections).toBe("number");
 });

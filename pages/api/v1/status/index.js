c0a81a08c8c33fd2f704bcac43c7d0f0301eed45
Éric Dagostim do Nascimento <69852409+EricDagostim@users.noreply.github.com>
2025-09-25 15:58:20 +0000

    Finish endpoint status da api

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -6,21 +6,26 @@ async function status(request, response) {
   const dataBaseVersionResult = await database.query("SHOW server_version;");
   const dataBaseValueVersion = dataBaseVersionResult.rows[0].server_version;
 
-  const dataBaseActiveConnectionsResult = await database.query(
-    `SELECT count(*) FROM pg_stat_activity;`,
-  );
+  const dataBaseMaxConnectionResult = await database.query("SHOW max_connections;");  
+  const dataBaseValueMaxConnections = dataBaseMaxConnectionResult.rows[0].max_connections;
 
-  const dataBaseValueActiveConnections =
-    dataBaseActiveConnectionsResult.rows[0].count;
+  const databaseName = process.env.POSTGRES_DB;
 
-  console.log(dataBaseValueActiveConnections);
+  const databaseOpenedConnectionsResult = 
+    await database.query({
+      text: "SELECT count(*)::int FROM pg_stat_activity WHERE datname = $1;",
+      values: [databaseName]      
+    });
+
+  const databaseValueOpenedConnections = databaseOpenedConnectionsResult.rows[0].count;
 
   response.status(200).json({
     updated_at: updatedAt,
     dependencies: {
       database: {
         version: dataBaseValueVersion,
-        active_connections: parseInt(dataBaseValueActiveConnections),
+        max_connections: parseInt(dataBaseValueMaxConnections),
+        opened_connections: databaseValueOpenedConnections,
       },
     },
   });
